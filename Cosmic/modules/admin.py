f7274cf9c37eaa941947b437be332f9a16e06d72
TAMILVIP007 <79161058+TAMILVIP007@users.noreply.github.com>
2022-08-31 11:46:09 +0000

    b

--- Cosmic/modules/admin.py
+++ Cosmic/modules/admin.py
@@ -1,5 +1,5 @@
 from telethon.tl.functions.channels import EditBannedRequest
-
+froom telethon.tl.types import ChatBannedRights
 from Cosmic import cosmo
 from Cosmic.functions.handler import cosmic
 from Cosmic.functions.misc import eor, get_user
